dc9db181fb9432fce24db29b9231a8660f2e083a
Pedro Vargas <pedro@fusefinance.com>
2025-10-16 08:54:35 -0500

    Testing two

--- .github/actions/run-node-script/index.js
+++ .github/actions/run-node-script/index.js
@@ -2,5 +2,7 @@ const body = process.argv[2];
 
 console.log('Hello, world!', {
   raw: body,
-  body: body.replace('\\\\r\\\\n', '\n'),
+  body: body
+    .replace(/\uFEFF/g, '') // BOM
+    .replace(/\r\n?/g, '\n'), // CRLF -> LF
 });
