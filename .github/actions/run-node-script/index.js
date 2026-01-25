89fe6dbce522e0db1576597ecb63465e2fd484d4
Pedro Vargas <pedro@fusefinance.com>
2026-01-25 12:44:18 -0300

    wip

---
+++ .github/actions/run-node-script/index.js
@@ -0,0 +1,13 @@
+import core from '@actions/core';
+
+async function run() {
+  exec(`npx bintest`, (error, stdout) => {
+    if (error !== null) {
+      core.setFailed(error);
+      core.notice(stdout);
+    }
+    core.info(stdout);
+  });
+}
+
+run();
