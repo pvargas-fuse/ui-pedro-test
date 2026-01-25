832a457f58c9dbb41c2e79f4e19c3fa8c2a9034d
Pedro Vargas <pedro@fusefinance.com>
2026-01-25 13:01:22 -0300

    wip

--- .github/actions/run-node-script/index.js
+++ .github/actions/run-node-script/index.js
@@ -2,7 +2,7 @@ const { exec } = require('node:child_process');
 const core = require('@actions/core');
 
 async function run() {
-  exec('npx bin-test', (error, stdout) => {
+  exec('npx bin-test sum 2 3', (error, stdout) => {
     if (error !== null) {
       core.setFailed(error);
       core.notice(stdout);
